28f7b2d2c9d858350a6bd417ca62f80ce2ebc34f
LAPTOP-UDC37R84\kwonl <Kwonlokyoung@gmail.com>
2024-03-16 14:54:53 -0700

    Commit

--- quiz.js
+++ quiz.js
@@ -1,17 +1,26 @@
-const qustions = [
+const questions = [
     {
-        question: "",
+        question: "This is a question",
         answers:[
             {text: "answer", correct: true},
             {text: "answer", correct: false},
             {text: "answer", correct: false},
             {text: "answer", correct: false},
         ]
-    }
+    },
+    {
+        question: "This is a question 1",
+        answers:[
+            {text: "answer1", correct: false},
+            {text: "answer1", correct: true},
+            {text: "answer1", correct: false},
+            {text: "answer1", correct: false},
+        ]
+    },
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons")
+const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -21,7 +30,7 @@ let score = 0;
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Next"
+    nextButton.innerHTML = "next";
     showQuestion();
 }
 
@@ -29,22 +38,67 @@ function showQuestion(){
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.
-    question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    currentQuestion.answers.array.forEach(answer => {
+    currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);  
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);  
     });
 }
 
-resetState(){
+function resetState(){
     nextButton.style.display = "none";
     while(answerButton.firstChild){
         answerButton.removeChild(answerButton.firstChild);
     }
 }
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Train Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
 
 startQuiz();
