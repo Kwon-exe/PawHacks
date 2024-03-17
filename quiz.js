b0139796b37754365d198a20b367915d7544d723
LAPTOP-UDC37R84\kwonl <Kwonlokyoung@gmail.com>
2024-03-16 17:13:22 -0700

    Added Timer!

--- quiz.js
+++ quiz.js
@@ -26,23 +26,35 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
-// let [seconds, minutes] = [0,0];
-// let timeRef = document.querySelector(".timerCount");
-// let int = null;
-//
-// function displayTimer(){
-//     seconds += 10;
-//     if(seconds === 1){
-//         seconds++;
-//     }
-//     if(seconds === 60){
-//         seconds = 0;
-//         minutes++;
-//     }
-//     let m = minutes < 10 ? "0" + minutes : minutes;
-//     let s = seconds < 10 ? "0" + seconds : minutes;
-//     timeRef.innerHTML = `${m} : ${s}`
-// }
+let [seconds, tenSeconds,minutes] = [0,0,0];
+let displayTime = document.getElementById("timerCount");
+let timer = null;
+
+function stopwatch(){
+    seconds++
+    if(seconds === 10){
+        seconds = 0;
+        tenSeconds++;
+    }
+    if(tenSeconds === 6){
+        tenSeconds = 0;
+        minutes++
+    }
+    displayTime.innerHTML = minutes + ":" + tenSeconds + seconds;
+}
+function watchStart(){
+    seconds = 0;
+    tenSeconds = 0;
+    minutes = 0;
+    if(timer!== null){
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000);
+}
+
+function watchStop(){
+    clearInterval(timer);
+}
 
 function startQuiz(){
     currentQuestionIndex = 0;
@@ -52,6 +64,7 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    watchStart();
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
@@ -70,6 +83,7 @@ function showQuestion(){
 }
 
 function resetState(){
+    document.getElementById("timerCount").style.display = "block";
     nextButton.style.display = "none";
     while(answerButton.firstChild){
         answerButton.removeChild(answerButton.firstChild);
@@ -77,6 +91,7 @@ function resetState(){
 }
 
 function selectAnswer(e){
+    watchStop();
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
@@ -105,8 +120,10 @@ function handleNextButton(){
     currentQuestionIndex++;
     if(currentQuestionIndex < questions.length){
         showQuestion();
+        watchStart();
     }else{
         showScore();
+        document.getElementById("timerCount").style.display = "none";
     }
 }
 
